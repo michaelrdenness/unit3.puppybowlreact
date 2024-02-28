6251d8380ca8e8b96ade5070fb62dc427e8584a7
Michael Denness <thisisturboproductions@gmail.com>
2024-02-28 16:59:35 -0500

    Finished Project

---
+++ src/API/index.js
@@ -0,0 +1,28 @@
+import axios from "axios"
+
+const BASE_URL = 'https://fsa-puppy-bowl.herokuapp.com/api/daxkerchner/players'
+
+// not using export default
+// we're just going to export multiple things
+// and no special default export
+
+// could do: export function getAllPlayers() {
+export const getAllPlayers = async () => {
+  const { data } = await axios.get(BASE_URL)
+  console.log('data', data)
+  return data.data.players
+}
+
+export const getSinglePlayer = async (playerId) => {
+  const { data } = await axios.get(BASE_URL + '/' + playerId)
+  console.log('data', data)
+  return data.data.player
+}
+
+export const addPlayer = async (player) => {
+  await axios.post(BASE_URL, player)
+}
+
+export const deletePlayer = async (playerId) => {
+  await axios.delete(BASE_URL + '/' + playerId)
+}
